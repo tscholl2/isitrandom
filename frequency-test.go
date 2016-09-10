87c43e28e67d3a40e13e86748dbac5ee5e55fb84
Travis <tscholl2@uw.edu>
2016-09-10 11:21:15 -0700

    add separate N method and read until end of file

--- frequency-test.go
+++ frequency-test.go
@@ -5,20 +5,27 @@ import (
 	"io"
 )
 
-const frequencyTestN = 10000
-
-// FrequencyTest tests the number of 1s and 0s it sees
-// and compares it to the expected number.
-// See http://cacr.uwaterloo.ca/hac/about/chap5.pdf pg 181
+// FrequencyTest calls FrequencyTestN with N = 10000.
 func FrequencyTest(rng io.Reader) float64 {
+	return FrequencyTestN(rng, 10000)
+}
+
+// FrequencyTestN tests the number of 1s and 0s it sees
+// and compares it to the expected number. It will read N
+// *bytes*. If N = -1, it will read until EOF.
+// See http://cacr.uwaterloo.ca/hac/about/chap5.pdf pg 181.
+func FrequencyTestN(rng io.Reader, N int) float64 {
 	ones := float64(0)
 	b := make([]byte, 1)
-	for i := 0; i < frequencyTestN; i++ {
+	bytesRead := 0
+	var err error
+	for bytesRead < N || (N < 0 && err != io.EOF) {
 		if _, err := rng.Read(b); err != nil && err != io.EOF {
 			panic(err)
-		} else if err == io.EOF && i < frequencyTestN-1 {
-			panic(fmt.Errorf("not enough bytes: read %d, want: %d", i, frequencyTestN))
+		} else if N > -1 && err == io.EOF && bytesRead < N-1 {
+			panic(fmt.Errorf("not enough bytes: read %d, want: %d", bytesRead, N))
 		}
+		bytesRead++
 		for b[0] > 0 {
 			if b[0]&1 == 1 {
 				ones++
@@ -26,7 +33,7 @@ func FrequencyTest(rng io.Reader) float64 {
 			b[0] = b[0] >> 1
 		}
 	}
-	n := float64(8 * frequencyTestN)
+	n := float64(8 * bytesRead)
 	zeros := n - ones
 	return chisquared((zeros-ones)*(zeros-ones)/n, 1)
 }
