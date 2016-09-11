16e99a2f3e0b1befca32a21781bf8231760307ca
Zack Scholl <Zack Scholl>
2016-09-11 18:12:11 -0400

    Updated serial stuff

--- serial.go
+++ serial.go
@@ -5,62 +5,56 @@ import (
 	"io"
 )
 
+type serial struct {
+	n00, n01, n10, n11 float64
+	started            bool
+	lastBit            bool
+}
+
+func (f *serial) next(b byte) {
+	r := New(bytes.NewBuffer([]byte{b}))
+	for j := 0; j < 8; j++ {
+		bit, _ := r.ReadBit()
+		// if bit {
+		// 	fmt.Printf("1")
+		// } else {
+		// 	fmt.Printf("0")
+		// }
+		if !f.started {
+			f.started = true
+			f.lastBit = bit
+			continue
+		}
+		if !f.lastBit && !bit { // 00
+			f.n00++
+		} else if !f.lastBit && bit { // 01
+			f.n01++
+		} else if f.lastBit && !bit { // 10
+			f.n10++
+		} else if f.lastBit && bit { // 11
+			f.n11++
+		}
+		f.lastBit = bit
+	}
+}
+
+func (f *serial) statistic() float64 {
+	n := f.n00 + f.n01 + f.n10 + f.n11 + 1
+	n0 := f.n00 + f.n01
+	n1 := f.n10 + f.n11 + 1
+	statistic := 4.0/(n-1)*(f.n00*f.n00+f.n01*f.n01+f.n10*f.n10+f.n11*f.n11) - (2/n)*(n0*n0+n1*n1) + 1
+	return statistic
+}
+
+func (f *serial) p() float64 {
+	s := f.statistic()
+	return chisquared(s, 2)
+}
+
 // SerialTest tests whether the number of occurences of
 // 00, 01, 10, and 11 as subsequences are approximately the same
 // as would be expected for a random sequence.
 // See http://cacr.uwaterloo.ca/hac/about/chap5.pdf pg 181
 func SerialTest(rng io.Reader) float64 {
-	N := 100
-	b := make([]byte, N)
-	for i := 0; i < N; i++ {
-		rng.Read(b)
-	}
-	pValue, _ := SerialTestN(bytes.NewBuffer(b))
-	return pValue
-}
-
-// SerialTestN tests the number of 2-bit subsequences
-// and compares it to the expected number. It will read N
-// *bytes*. If N = -1, it will read until EOF.
-// See http://cacr.uwaterloo.ca/hac/about/chap5.pdf pg 181.
-func SerialTestN(rng io.Reader) (float64, float64) {
-	// counts           00 01 10 11
-	counts := []float64{0, 0, 0, 0}
-	started := false
-	lastBit := false
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(rng)
-	data := buf.Bytes()
-	r := New(bytes.NewBuffer(data))
-	for i := 0; i < len(data); i++ {
-		for j := 0; j < 8; j++ {
-			bit, _ := r.ReadBit()
-			// if bit {
-			// 	fmt.Printf("1")
-			// } else {
-			// 	fmt.Printf("0")
-			// }
-			if !started {
-				started = true
-				lastBit = bit
-				continue
-			}
-			if !lastBit && !bit { // 00
-				counts[0]++
-			} else if !lastBit && bit { // 01
-				counts[1]++
-			} else if lastBit && !bit { // 10
-				counts[2]++
-			} else if lastBit && bit { // 11
-				counts[3]++
-			}
-			lastBit = bit
-		}
-	}
-
-	n := counts[0] + counts[1] + counts[2] + counts[3] + 1
-	n0 := counts[0] + counts[1]
-	n1 := counts[2] + counts[3] + 1
-	statistic := 4/(n-1)*(counts[0]*counts[0]+counts[1]*counts[1]+counts[2]*counts[2]+counts[3]*counts[3]) - (2/n)*(n0*n0+n1*n1) + 1
-	return chisquared(statistic, 2), statistic
+	return buildIOReaderTest(&serial{})(rng)
 }
--- serial_test.go
+++ serial_test.go
@@ -3,29 +3,22 @@ package isitrandom
 import "testing"
 
 func TestSerialTest(t *testing.T) {
-	/*
-		t.Run("correct on small input", func(t *testing.T) {
-			var buf = bytes.NewBuffer([]byte{0x00})
-			_, statistic := SerialTestN(buf, 1)
-			if statistic != 64.0/8 {
-				t.Errorf("got: %f explected: %f", statistic, 64.0/8)
-			}
-		})
-		t.Run("reads whole ", func(t *testing.T) {
-			var buf = bytes.NewBuffer([]byte{0x00, 0xff})
-			_, statistic := SerialTestN(buf, -1)
-			if statistic != 0 {
-				t.Errorf("got: %f explected: %d", statistic, 0)
-			}
-		})
-		t.Run("panics on not enough values", func(t *testing.T) {
-			defer func() {
-				if err := recover(); err == nil {
-					t.Errorf("expected a panic, but didnt happen")
-				}
-			}()
-			var buf = bytes.NewBuffer([]byte{0x00, 0xff})
-			SerialTestN(buf, 3)
-		})
-	*/
+	t.Run("correct on small input", func(t *testing.T) {
+		ft := &serial{}
+		short := []byte{
+			0xe3, 0x11, 0x4e, 0xf2, 0x49, 0xe3, 0x11, 0x4e,
+			0xf2, 0x49, 0xe3, 0x11, 0x4e, 0xf2, 0x49, 0xe3,
+			0x11, 0x4e, 0xf2, 0x49}
+		for _, val := range short {
+			ft.next(val)
+		}
+		statistic := ft.statistic()
+		if statistic-0.625157 > 0.00001 {
+			t.Errorf("got: %f explected: %f", statistic, 0.625157)
+		}
+		pValues := ft.p()
+		if pValues-0.9 > 0.00001 {
+			t.Errorf("got: %f explected: %f", pValues, 0.9)
+		}
+	})
 }
